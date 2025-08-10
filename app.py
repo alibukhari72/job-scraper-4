13bb55e2647a806a2ee2482db4d2a776c37aaf81
alibukhari72 <alimurtazabukhari110@gmail.com>
2025-08-10 07:37:33 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from scraper import scrape_jobs
 from flask import Flask
 app = Flask(_name_)
 
@@ -6,3 +7,5 @@ def run_scraper():
     # 🟢 Import and run your scraper function here
     result = scrape_jobs()  # replace with your actual scraper function name
     return f"Scraping done. Found {len(result)} jobs."
+if _name_ == '_main_':
+    app.run(host='0.0.0.0', port=5000)
